8f68bc4f5ea5d9474dc8c7ef0d96533ad6e8551e
Akhil Adusumilli <akhiladusumilli100@gmail.com>
2025-02-11 19:21:23 -0500

    add task1.sh

---
+++ task1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Find the process ID (PID) of infinite.sh
+PID=$(ps | grep 'sh' | awk '{print $1}')
+
+# Kill the process if found
+if [ ! -z "$PID" ]; then
+    kill -9 $PID
+    echo "Killed infinite.sh (PID: $PID)"
+else
+    echo "No running infinite.sh process found."
+fi
